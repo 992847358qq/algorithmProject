f3c60fde5ebbeb614238939152dbfe450603bb61
wanggaoliang <wanggaoliang@xueleyun.com>
2019-09-27 15:37:16 +0800

    第一次提交信息

---
+++ src/main/java/First/First.java
@@ -0,0 +1,12 @@
+package First;
+
+/**
+ * @Author: wgl
+ * @Date: 2019/9/27 15:36
+ * @Description:
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
