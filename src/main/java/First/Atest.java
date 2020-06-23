fbb311041b17489867d5a142023737748e2602bb
wanggaoliang <wanggaoliang@xueleyun.com>
2020-06-23 21:14:27 +0800

    练习

--- src/main/java/First/Atest.java
+++ src/main/java/First/Atest.java
@@ -1,5 +1,8 @@
 package First;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: wgl
  * @Date: 2020/05/14 13:47
@@ -7,13 +10,18 @@ package First;
  */
 public class Atest {
     public static void main(String[] args) {
-        String str = "[ zhǐ hǎo ]";
+        /*String str = "[ zhǐ hǎo ]";
         if (str.length()>1){
             String string = str.substring(1,str.length()-1);
             String[] s = string.split(" ");
             for (String s1 : s) {
                 System.out.println(s1);
             }
+        }*/
+        for (int i = 0;i<10;i+=2){
+            System.out.println(i);
         }
+        ThreadLocal threadLocal = new ThreadLocal();
+        List<String> list = new ArrayList<>();
     }
 }
---
+++ src/main/java/practice/T1.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * @Author: wgl
+ * @Date: 2020/06/23 21:13
+ * @Description:
+ */
+public class T1 {
+    public static void main(String[] args) {
+
+    }
+}
